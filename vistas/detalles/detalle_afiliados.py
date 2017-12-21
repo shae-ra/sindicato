de7959ddbeaafc8358bc354563acf3b7dc1ef264
Juanjo <juanjorc98@gmail.com>
2017-12-21 19:53:15 -0300

    tabWidget de detalle afiliado
    ahora cada vez que se abra la ventana se pone el índice del tabwidget en 0 (cero)

--- vistas/detalles/detalle_afiliados.py
+++ vistas/detalles/detalle_afiliados.py
@@ -21,8 +21,14 @@ from PyQt5 import uic
 #Creacion de la clase detalleAfiliados
 class DetalleAfiliados(QtWidgets.QWidget):
 	#Inicializacion del Objeto QWidget
-	def __init__(self):  
-		QWidget.__init__(self)  
-		
-		#Importamos la vista "detalleAfiliados" y la alojamos dentro de la variable "vistaDetalle"
-		widgetafiliado = uic.loadUi("gui/detalles/detalleAfiliados.ui", self)
+	def __init__(self):
+		QWidget.__init__(self)
+
+		# Importamos la vista "detalleAfiliados" y la alojamos dentro de la variable "vistaDetalle"
+		# Agregamos 'self.' al objeto así podemos acceder a él en el resto de las funciones
+		self.widgetafiliado = uic.loadUi("gui/detalles/detalleAfiliados.ui", self)
+
+	def showEvent(self, event):
+		self.widgetafiliado.tabWidget.setCurrentIndex(0)
+		# Accedo al objeto 'tabWidget' que es hijo de el objeto 'widgetafiliado' y además llamo a la función setCurrentIndex()
+		# la funcion setCurrentIndex pertence al último hijo llamado.
