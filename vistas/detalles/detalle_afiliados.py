1b1e410a681800e92aebea49936a472104d3b332
Juanjo <juanjorc98@gmail.com>
2018-01-04 15:45:09 -0300

    Cierre con escape.
    Las ventanas emergentes se cierran apretando la tecla escape.

--- vistas/detalles/detalle_afiliados.py
+++ vistas/detalles/detalle_afiliados.py
@@ -8,11 +8,12 @@
 import sys
 # Importamos las herramientas de PyQT que vamos a utilizar
 from PyQt5 import QtWidgets, uic, QtGui
-# Importamos los elementos que se encuentran dentro del diseñador 
+# Importamos los elementos que se encuentran dentro del diseñador
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QTabWidget
 from vistas.cargas import carga_debito
 # Importamos el modulo uic necesario para levantar un archivo .ui
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
 
 
 #====================
@@ -37,6 +38,10 @@ class DetalleAfiliados(QtWidgets.QWidget):
 		self.widgetafiliado.pushButton_ingresarDebito.clicked.connect(self.mostrarCarga)
 		# Accedo al objeto 'tabWidget' que es hijo de el objeto 'widgetafiliado' y además llamo a la función setCurrentIndex()
 		# la funcion setCurrentIndex pertence al último hijo llamado.
-	
+
 	def mostrarCarga(self):
 		self.widgetdecarga.show()
+
+	def keyPressEvent(self, event):
+		if event.key() == Qt.Key_Escape:
+			self.close()
--- vistas/detalles/detalle_proveedores.py
+++ vistas/detalles/detalle_proveedores.py
@@ -8,10 +8,11 @@
 import sys
 # Importamos las herramientas de PyQT que vamos a utilizar
 from PyQt5 import QtWidgets, uic, QtGui
-# Importamos los elementos que se encuentran dentro del diseñador 
+# Importamos los elementos que se encuentran dentro del diseñador
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QTabWidget
 # Importamos el modulo uic necesario para levantar un archivo .ui
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
 
 
 #====================
@@ -21,9 +22,13 @@ from PyQt5 import uic
 #Creacion de la clase detalleAfiliados
 class DetalleProveedores(QtWidgets.QWidget):
 	#Inicializacion del Objeto QWidget
-	def __init__(self):  
-		QWidget.__init__(self)  
-		
+	def __init__(self):
+		QWidget.__init__(self)
+
 		#Importamos la vista "detalleAfiliados" y la alojamos dentro de la variable "vistaDetalle"
 		self.widgetproveedor = uic.loadUi("gui/detalles/detalleProveedores.ui", self)
 		self.setWindowTitle("Consulta del Proveedor")
+
+	def keyPressEvent(self, event):
+		if event.key() == Qt.Key_Escape:
+			self.close()
