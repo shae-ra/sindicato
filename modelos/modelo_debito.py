2c6f66684380b0888326380ec3dbecf88faf0d48
Juanjo <juanjorc98@gmail.com>
2018-04-13 14:08:14 -0300

    Correcciones para arrancar con la alta de debito

--- modelos/modelo_debito.py
+++ modelos/modelo_debito.py
@@ -2,18 +2,23 @@ from PyQt5 import QtCore
 from libs.db import querier
 import cerberus
 
-class ModeloDescuentos(QtCore.QAbstractTableModel):
+class ModeloDebito(QtCore.QAbstractTableModel):
     q = querier.Querier()
     __v = cerberus.Validator()
 
     def __init__(self, propiedades = None, parent = None):
-        super(ModeloDescuentos, self).__init__()
+        super(ModeloDebito, self).__init__()
 
-        self.__esquemaDescuentos = {
+        self.__esquemaDebitos = {
+            'legajo_afiliado' : {'type' : 'integer', 'maxlength' : 8 },
+            'id_banco' : {'type' : 'integer', 'maxlength' : 8 },
+            'cbu' : { 'type': 'integer', 'maxlength' : 22},
+
+#Importado de ModeloDescuentos
             'id' : {'type' : 'integer', 'maxlength' : 32 },
             'mes_descuento' : {'type' : 'date' },
             'proveedor_id' : { 'type': 'integer', 'maxlength' : 16},
             'cuota_actual' : {'type' : 'integer', 'maxlength' : 2 },
             'total_cuotas' : {'type' : 'integer', 'maxlength' : 2 },
-            'fecha_carga_actual' : {'type' : 'date' },  
+            'fecha_carga_actual' : {'type' : 'date' },
         }
--- vistas/cargas/carga_debito.py
+++ vistas/cargas/carga_debito.py
@@ -8,11 +8,13 @@
 import sys
 # Importamos las herramientas de PyQT que vamos a utilizar
 from PyQt5 import QtWidgets, uic, QtGui
-# Importamos los elementos que se encuentran dentro del diseñador 
+# Importamos los elementos que se encuentran dentro del diseñador
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QTabWidget
 # Importamos el modulo uic necesario para levantar un archivo .ui
 from PyQt5 import uic
 
+from modelos.modelo_proveedor import ModeloProveedor
+from modelos.modelo_debito import ModeloDebito
 
 #====================
 #DEFINICION DE CLASES
@@ -25,4 +27,9 @@ class CargaDebito(QtWidgets.QWidget):
 		QWidget.__init__(self)
 
 		# Importamos la vista "carga_debito" y la alojamos dentro de la variable "carga"
-		self.carga = uic.loadUi("gui/cargas/carga_debito.ui", self)
+		self.v_carga = uic.loadUi("gui/cargas/carga_debito.ui", self)
+
+		self.model_prov = ModeloProveedor(['id'])
+		self.model = ModeloDebito()
+
+		self.v_carga.prov_id.setModel(self.model_prov)
