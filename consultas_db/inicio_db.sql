d35d8c512866888d7a00a7e8d06bbe32a43dba45
Juanjo <juanjorc98@gmail.com>
2018-04-18 17:41:27 -0300

    Base de datos actualizada.

--- consultas_db/inicio_db.sql
+++ consultas_db/inicio_db.sql
@@ -1,10 +1,11 @@
 use sindicato;
 
 CREATE TABLE afiliados (
-	legajo int(8) UNSIGNED NOT NULL PRIMARY KEY, # El legajo deberia ser un entero de 8 y no deberia permitir poner signos. Formato: 01002595
-    dni int(8) UNSIGNED UNIQUE KEY,
+	legajo varchar(22) UNSIGNED NOT NULL PRIMARY KEY, # El legajo deberia ser un entero de 8 y no deberia permitir poner signos. Formato: 01002595
+    dni varchar(8) UNSIGNED UNIQUE KEY,
     tipo_afiliado varchar(20) ,
-    cuil bigint(11) UNSIGNED UNIQUE KEY,
+		activo smallint(1),
+    cuil varchar(11) UNSIGNED UNIQUE KEY,
     apellido varchar(50), # Podemos poner 10 mas? por las dudas!! no recuerdo bien, pero creo que hay gente con doble apellido y largos encima. Por ejemplo: Lagos fuentealba Cristian Juan Jose
     nombre varchar(50), # Podemos poner 10 mas? por las dudas!! no recuerdo bien, pero creo que hay gente con doble apellido y largos encima. Por ejemplo: Lagos fuentealba Cristian Juan Jose
     fecha_nacimiento date,
@@ -12,7 +13,7 @@ CREATE TABLE afiliados (
     estado_civil varchar(20),
     nacionalidad varchar(20),
     calle varchar(80),
-    altura int(8),
+    altura varchar(8),
     piso varchar(10),
     depto varchar(4),
     cod_postal varchar(20), # Aca con un largo de 4 esta bien.
@@ -72,14 +73,17 @@ CREATE TABLE servicios_afiliado(
 );
 
 CREATE TABLE debitos(
-    id int(32) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    legajo_afiliado int(8) UNSIGNED NOT NULL,
-    mes_descuento date, # (Mes de descuento)
-    proveedor_id int(16) UNSIGNED NOT NULL,
+    id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    legajo_afiliado varchar(22) UNSIGNED NOT NULL,
+    fecha_descuento date, # (Mes de descuento)
+		fecha_carga_inicial date, # FIJARSE SI FUNCIONA
+
+    proveedor_id int(8) UNSIGNED NOT NULL,
     cuota_actual int(2),
     total_cuotas int(2),
-    fecha_carga_inicial date, # FIJARSE SI FUNCIONA
-	importe_total int(8)
+		importe_actual decimal(8,2),
+		importe_total decimal(8,2),
+		n_credito varchar(22)
 
 );
 
@@ -93,16 +97,16 @@ CREATE TABLE proveedores(
     nombre varchar(50),
     servicios varchar(100),
     calle varchar(80),
-    altura int(8),
+    altura int(8) DEFAULT NULL,
     localidad varchar(50),
 		telefono varchar(20),
     celular varchar(20),
     email varchar(80),
-    cuit bigint(11) UNSIGNED UNIQUE KEY,
+    cuit varchar(11) UNSIGNED UNIQUE KEY,
     razon_social varchar(60),
     cbu varchar(22), #solo un largo de 22. El CBU contiene 22 numeros y deberia poderse guardar siempre y cuando tenga los 22 numeros, de otra forma no tiene que ser posible guardar. Esto achicaria mucho el margen de error.
     banco varchar(60),
-    cuenta bigint(16),
+    cuenta varchar(16),
     comision varchar(40),
     responsable varchar(40),
     forma_pago varchar(40),
