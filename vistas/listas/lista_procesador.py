5dbbf267dca964a2a177890250df60c68cb9675e
Juanjo <juanjorc98@gmail.com>
2018-06-04 15:59:58 -0300

    obtener path al archivo bet

--- vistas/listas/lista_procesador.py
+++ vistas/listas/lista_procesador.py
@@ -8,7 +8,7 @@
 import sys
 # Importamos las herramientas de PyQT que vamos a utilizar
 from PyQt5 import QtWidgets, uic, QtGui
-# Importamos los elementos que se encuentran dentro del diseñador 
+# Importamos los elementos que se encuentran dentro del diseñador
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
 # Importamos el modulo uic necesario para levantar un archivo .ui
 from PyQt5 import uic
@@ -19,8 +19,22 @@ from PyQt5 import uic
 #====================
 
 #Creacion de la clase listaProveedores
-class ListaProcesador(QtWidgets.QWidget): 
-	def __init__(self):  
-		QWidget.__init__(self)  
+class ListaProcesador(QtWidgets.QWidget):
+	def __init__(self):
+		QWidget.__init__(self)
 		#Configuracion del archivo .ui
-		listaProcesador = uic.loadUi("gui/listas/listaProcesador.ui", self)
+		self.listaProcesador = uic.loadUi("gui/listas/listaProcesador.ui", self)
+
+		self.listaProcesador.btn_buscar_bet.clicked.connect(self.handleOpenBet)
+
+	def handleOpenBet(self):
+
+		path = QtWidgets.QFileDialog.getOpenFileName(
+			self, 'Open File', "", "Texto plano (*.txt)"
+		)
+
+		print(path[0])
+
+		bet_file = open(path[0], "r")
+
+		bet_file.close()
