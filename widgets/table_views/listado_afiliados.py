9cf0644a068dc490e16881c425ee1a86bca65582
Juanjo <juanjorc98@gmail.com>
2018-06-20 11:30:52 -0300

    Prototipo de menu contextual en tabla de afiliados

---
+++ widgets/table_views/listado_afiliados.py
@@ -0,0 +1,26 @@
+from PyQt5 import QtWidgets, QtGui
+
+class ListadoAfiliadosContextM(QtWidgets.QTableView):
+
+	def __init__(self, parent = None):
+		super(ListadoAfiliadosContextM, self).__init__(parent)
+		self.parent = parent
+
+	def contextMenuEvent(self, event):
+		self.menu = QtWidgets.QMenu(self)
+		bajaAfiliado = QtWidgets.QAction('Baja de Afiliado', self)
+		bajaAfiliado.triggered.connect(lambda: self.bajaAfiliado(event))
+		self.menu.addAction(bajaAfiliado)
+		# add other required actions
+		if self.selectedIndexes():
+			self.menu.popup(QtGui.QCursor.pos())
+
+
+	def bajaAfiliado(self, event):
+		print ("bajaAfiliado slot called")
+		indexes = self.selectedIndexes()
+		# get the selected row and column
+		for index in indexes:
+			print(self.model().itemData(index))
+
+		# print("Row: {} - Col: {}".format(row, col))
